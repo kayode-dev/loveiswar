823712ee029ef1bc791bc53ef588e19b9a5038bd
kayode <babalolakayode44@gmail.com>
2025-01-16 16:22:30 +0100

    update favicon and remove font giest font family

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] text-red-700 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] text-red-700 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <p className="text-xl">starsamm presents....</p>
       <p className="text-3xl">LOVE IS WAR</p>
       <p className="text-xl">...coming soon...</p>
