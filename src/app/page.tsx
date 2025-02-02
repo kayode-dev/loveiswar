08586971e4532bf393ea4b46a12bc713c63ecc03
Kayode Babalola <babalolakayode44@gmail.com>
2025-02-02 22:02:56 +0100

    add loading state and transition effect for homepage image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
+"use client";
 import Image from "next/image";
 import loveIsWarGrunge from "@/assets/Love is war Grunge.gif";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+  }, []);
   return (
-    <div className="h-dvh flex items-center justify-center">
+    <div className="h-dvh flex items-center justify-center ">
       <Image
         src={loveIsWarGrunge}
         alt="love is war logo"
-        className="relative -left-2 md:w-1/2"
+        className={cn(
+          "relative -left-2 md:w-1/2 opacity-0 duration-[2s] ease-in transition-opacity",
+          {
+            "opacity-100": isLoading,
+          }
+        )}
       />
     </div>
   );
